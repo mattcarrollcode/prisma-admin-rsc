3db6e6b69b0318f1e08427e1a67db0df7ff4f1df
Matt Carroll <mattca@fb.com>
2023-03-29 19:23:45 -0700

    first working version

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
 import { prisma } from '../lib/db'
-import { inspect } from 'util'
-import PrismaAdmin from '../components/prisma-admin'
+import { use } from 'react'
 
-export default async function Home() {
+export default async function PrismaAdmin() {
+  const tableNames = Object.keys(prisma).filter(attr => {
+    if (!attr.startsWith("$") && !attr.startsWith("_")) {
+      return attr
+    }
+  })
   {/* @ts-expect-error Server Component */}
-  return <PrismaAdmin prisma={prisma} />
+  return (
+    <>
+      <h1>Prisma Admin</h1>
+      {tableNames.map((tableName, index) => (
+        <PrismaTable key={index} tableName={tableName} />
+        ))}
+    </>
+  );
 }
+
+async function PrismaTable({tableName}){
+  const rows = await prisma[tableName].findMany({take: 10})
+  const columnNames = rows.length > 0 ? Object.keys(rows[0]) : [];
+  return (
+    <div>
+      <h2>{tableName}</h2>
+      <table>
+        <thead>
+          <tr>
+            {columnNames.map((columnName, index) => (
+              <th key={index}>{columnName}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {columnNames.map((columnName, colIndex) => (
+                <td key={`${rowIndex}-${colIndex}`}>
+                  {row[columnName] != null ? row[columnName].toString() : ''}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
